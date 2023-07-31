b169b2ccd9800d8104a8b960fbf01d83cd6f1dff
Tom Waterman <tjwaterman99@gmail.com>
2023-07-31 17:30:47 +0200

    Add limit

--- pages/history.vue
+++ pages/history.vue
@@ -4,7 +4,7 @@ let user = getUser().load()
 
 
 async function loadData() {
-    let resp = await client.from('game_history').select('*').eq('user_id', user.id).order('created_at', {ascending: false})
+    let resp = await client.from('game_history').select('*').eq('user_id', user.id).order('created_at', {ascending: false}).limit(50)
     if (resp.status == 200) {
         return resp.data
     } else {
